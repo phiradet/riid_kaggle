600f9a2d4884cde531b244013eba615d3284c6fb
phiradet <phiradet@gmail.com>
2020-12-05 23:53:29 +0900

    Densify dataset

---
+++ scripts/densify_dataset.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+import fire
+import torch
+from tqdm.auto import tqdm
+
+
+def main(src_dir: str, dst_dir: str):
+    all_files = list(sorted(glob.glob(os.path.join(src_dir, "*pth"))))
+
+    for f in tqdm(all_files):
+        data = torch.load(f)
+
+        data["y"] = data.to_dense()
+        data["feature"] = data.to_dense()
+
+        basename = os.path.basename(f)
+        torch.save(data, os.path.join(dst_dir, basename))
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
