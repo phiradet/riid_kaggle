f8e092e4f166fb939ac8dd2f7bf69bff31c76ea6
phiradet <phiradet@gmail.com>
2020-12-10 21:31:02 +0900

    RNN state management

--- libs/inferencer.py
+++ libs/inferencer.py
@@ -6,10 +6,96 @@ from typing import *
 import torch
 import pandas as pd
 from torch.nn.utils.rnn import pad_sequence
+from libs.modules.stacked_augmented_lstm import TensorPair
 
 from libs.feature import extract_feature
 from libs.model import Predictor
 from libs.utils.io import load_state_dict
+
+
+class RNNState(object):
+
+    def __init__(self,
+                 known_user_id_idx: Dict[int, int],
+                 h_t: torch.tensor,
+                 c_t: torch.tensor):
+
+        self.known_user_id_idx = known_user_id_idx
+        self.h_t = h_t
+        self.c_t = c_t
+
+    @classmethod
+    def from_file(cls,
+                  initial_state_dir: Optional[str],
+                  lstm_num_layers: int,
+                  lstm_hidden_dim: int):
+        if initial_state_dir is None:
+            known_user_id_idx = {}
+            h_t = torch.zeros(lstm_num_layers, 0, lstm_hidden_dim)
+            c_t = torch.zeros(lstm_num_layers, 0, lstm_hidden_dim)
+        else:
+            known_user_id_idx = pickle.load(open(os.path.join(initial_state_dir, "user_id_idx.pickle"), "rb"))
+            h_t = torch.load(os.path.join(initial_state_dir, "h_t.pth"))
+            c_t = torch.load(os.path.join(initial_state_dir, "c_t.pth"))
+
+        return cls(known_user_id_idx, h_t, c_t)
+
+    def update_state(self, user_ids: List[int], states: TensorPair):
+
+        selection_ids = []
+
+        for idx, user_id in enumerate(user_ids):
+            if user_id in self.known_user_id_idx:
+                selection_ids.append(self.known_user_id_idx[user_id])
+            else:
+                new_user_idx = len(self.known_user_id_idx)
+                selection_ids.append(new_user_idx)
+                self.known_user_id_idx[user_id] = new_user_idx
+
+        selection_ids = torch.tensor(selection_ids, dtype=torch.long)
+
+        num_layers, known_user_count, hidden_size = self.h_t.shape
+        unknown_user_count = len(self.known_user_id_idx) - known_user_count
+
+        zero_tensor = torch.zeros(num_layers, unknown_user_count, hidden_size, dtype=self.h_t.dtype)
+        self.h_t = torch.cat([self.h_t, zero_tensor], dim=1)
+        self.c_t = torch.cat([self.c_t, zero_tensor], dim=1)
+
+        self.h_t[:, selection_ids, :] = states[0]
+        self.c_t[:, selection_ids, :] = states[1]
+
+        assert self.h_t.shape[1] == len(self.known_user_id_idx)
+        assert self.c_t.shape[1] == len(self.known_user_id_idx)
+
+    def get_state(self, user_ids: List[int]) -> Optional[TensorPair]:
+        if len(self.known_user_id_idx) == 0:
+            return None
+        else:
+            selection_ids = []
+
+            for idx, user_id in enumerate(user_ids):
+                selection_ids.append(self.known_user_id_idx.get(user_id, -1))
+
+            selection_ids = torch.tensor(selection_ids, dtype=torch.long)
+            known_user_mask = selection_ids >= 0
+
+            known_user_h_t = self.h_t[:, selection_ids[known_user_mask], :]
+            known_user_c_t = self.c_t[:, selection_ids[known_user_mask], :]
+
+            num_layers, _, hidden_size = self.h_t.shape
+            output_h_t = torch.zeros(num_layers, len(user_ids), hidden_size, dtype=self.h_t.dtype)
+            output_c_t = torch.zeros(num_layers, len(user_ids), hidden_size, dtype=self.c_t.dtype)
+
+            output_h_t[:, known_user_mask, :] = known_user_h_t
+            output_c_t[:, known_user_mask, :] = known_user_c_t
+
+            if output_h_t.dtype is not torch.float:
+                output_h_t = output_h_t.float()
+
+            if output_c_t.dtype is not torch.float:
+                output_c_t = output_c_t.float()
+
+            return output_h_t, output_c_t
 
 
 class Inferencer(object):
@@ -24,20 +110,16 @@ class Inferencer(object):
         self.model_config = model_config
         self.model = Predictor(**model_config)
 
-        self.part_idx = pickle.load(open(os.path.join(idx_map_dir, f"part_idx.pickle"), "rb"))
-        self.type_idx = pickle.load(open(os.path.join(idx_map_dir, f"type_idx.pickle"), "rb"))
-        self.bundle_id_idx = pickle.load(open(os.path.join(idx_map_dir, f"bundle_id_idx.pickle"), "rb"))
-        self.content_id_idx = pickle.load(open(os.path.join(idx_map_dir, f"content_id_idx.pickle"), "rb"))
-        self.seq_len = seq_len
+        if idx_map_dir is not None:
+            self.part_idx = pickle.load(open(os.path.join(idx_map_dir, f"part_idx.pickle"), "rb"))
+            self.type_idx = pickle.load(open(os.path.join(idx_map_dir, f"type_idx.pickle"), "rb"))
+            self.bundle_id_idx = pickle.load(open(os.path.join(idx_map_dir, f"bundle_id_idx.pickle"), "rb"))
+            self.content_id_idx = pickle.load(open(os.path.join(idx_map_dir, f"content_id_idx.pickle"), "rb"))
 
-        if initial_state_dir is None:
-            self.training_user_id_idx = {}
-            self.h0 = None
-            self.c0 = None
-        else:
-            self.training_user_id_idx = pickle.load(open(os.path.join(initial_state_dir, "user_id_idx.pickle"), "rb"))
-            self.h0 = torch.load(os.path.join(initial_state_dir, "h0.pth"))
-            self.c0 = torch.load(os.path.join(initial_state_dir, "c0.pth"))
+        self.seq_len = seq_len
+        self.rnn_state = RNNState.from_file(initial_state_dir=initial_state_dir,
+                                            lstm_hidden_dim=self.model_config["lstm_hidden_dim"],
+                                            lstm_num_layers=self.model_config["lstm_num_layers"])
 
         state_dict = load_state_dict(checkpoint_dir)
         self.model.load_state_dict(state_dict)
@@ -61,14 +143,11 @@ class Inferencer(object):
 
         return pd.Series(output)
 
-    def update_state(self, user_ids: List[str], states: torch.tensor):
-        pass
+    def update_state(self, user_ids: List[str], states: TensorPair):
+        self.rnn_state.update_state(user_ids, states)
 
-    def get_state(self, user_ids: List[str]):
-        if len(self.training_user_id_idx) == 0:
-            return None
-        else:
-            raise NotImplementedError
+    def get_state(self, user_ids: List[str]) -> Optional[TensorPair]:
+        return self.rnn_state.get_state(user_ids)
 
     def predict(self, test_df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
         df = test_df.groupby("user_id").apply(self.aggregate)
@@ -92,6 +171,7 @@ class Inferencer(object):
             if verbose:
                 print("feature_tensor", feature_tensor.shape)
                 print("content_id_tensor", content_id_tensor.shape)
+
             pred_logit, state = self.model(content_id=content_id_tensor,
                                            bundle_id=None,
                                            feature=feature_tensor,
