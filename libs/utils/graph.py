33ca2c879fe0a945fa878750237d8872d76ca834
phiradet <phiradet@gmail.com>
2020-12-11 23:06:01 +0900

    Test graph creation

--- libs/utils/graph.py
+++ libs/utils/graph.py
@@ -1,19 +1,14 @@
 from typing import *
-from math import copysign
 
 import torch
-from tqdm.auto import tqdm
-
-from libs.utils.io import read_contents
+import pandas as pd
 
 
-def get_content_adjacency_matrix(questions_path: str,
-                                 lectures_path: str,
+def get_content_adjacency_matrix(content_df: pd.DataFrame,
                                  content_id_idx: Dict[int, int],
-                                 no_question_lecture_link: bool = True) -> torch.tensor:
+                                 no_question_lecture_link: bool = True,
+                                 no_self_loop: bool = True) -> torch.tensor:
     # edge = overlapping tags
-    content_df = read_contents(questions_path=questions_path,
-                               lectures_path=lectures_path)
     content_df["idx"] = content_df["content_id"].apply(lambda id: content_id_idx[id])
     content_count = len(content_id_idx)
 
@@ -23,10 +18,28 @@ def get_content_adjacency_matrix(questions_path: str,
                           requires_grad=False)
 
     exploded_content_df = content_df[["idx", "tags"]].explode("tags")
+    exploded_content_df = exploded_content_df[exploded_content_df["tags"].notnull()]
+    exploded_content_df["tags"] = exploded_content_df["tags"].astype(int)
+
     edge_indices = exploded_content_df \
-        .merge(exploded_content_df, on="tags")[["idx_x", "idx_y"]] \
-        .drop_duplicates() \
-        .values
+        .merge(exploded_content_df, on="tags")
+
+    if no_question_lecture_link:
+
+        def get_sign(num):
+            if num >= 0:
+                return 1
+            else:
+                return -1
+
+        same_content_type_mask = edge_indices["idx_x"].apply(get_sign) == edge_indices["idx_y"].apply(get_sign)
+        edge_indices = edge_indices[same_content_type_mask]
+
+    if no_self_loop:
+        no_self_loop_mask = edge_indices["idx_x"] != edge_indices["idx_y"]
+        edge_indices = edge_indices[no_self_loop_mask]
+
+    edge_indices = edge_indices[["idx_x", "idx_y"]].drop_duplicates().values
 
     adj_mat[edge_indices[:, 0], edge_indices[:, 1]] = 1.
     adj_mat[edge_indices[:, 1], edge_indices[:, 0]] = 1.
